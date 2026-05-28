ca9c32a5fd011c5f5a9ab1bab42ba1346b179f39
robird <dgcergo@163.com>
2026-05-29 03:17:05 +0800

    feat: 新增TextAdv2项目及相关文件，包含Location类和主程序

---
+++ prototypes/TextAdv2/Location.cs
@@ -0,0 +1,10 @@
+
+// using Atelia.StateJournal;
+
+// namespace Atelia.TextAdv2;
+
+// class Location {
+//     private DurableDict<string> _data;
+//     public string Id {get;init;}
+//     // public I
+// }
